3c0988b37c4094756f84bcf9f128a13e7e1d20a3
KONSTANTIN <kuznetsovkm@yandex.ru>
2026-01-07 09:11:56 +0300

    Полный расчет цистерна с ЛВЖ

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import json
 import sqlite3
 from pathlib import Path
 from calculations import equipment_type_0_kind_0, equipment_type_1_kind_0, equipment_type_2_kind_0, \
-    equipment_type_3_kind_0, equipment_type_4_kind_0, equipment_type_5_kind_2, equipment_type_6_kind_0
+    equipment_type_3_kind_0, equipment_type_4_kind_0, equipment_type_5_kind_2, equipment_type_6_kind_0, \
+    equipment_type_7_kind_0
 
 TYPICAL_SCENARIOS_PATH = Path("data/calc/typical_scenarios.json")  # при необходимости поправьте
 DB_PATH = Path("data/iris.sqlite3")  # при необходимости поправьте
@@ -126,6 +127,8 @@ def main(db_path: Path = DB_PATH, typical_scenarios_path: Path = TYPICAL_SCENARI
                     payload = equipment_type_4_kind_0.calc_for_scenario(equipment, substance, sc, scenario_no_global)
                 elif equipment["equipment_type"] == 6 and substance["kind"] == 0:
                     payload = equipment_type_6_kind_0.calc_for_scenario(equipment, substance, sc, scenario_no_global)
+                elif equipment["equipment_type"] == 7 and substance["kind"] == 0:
+                    payload = equipment_type_7_kind_0.calc_for_scenario(equipment, substance, sc, scenario_no_global)
 
 
                 elif equipment["equipment_type"] == 5 and substance["kind"] == 2:
