d10b59715906d6e92e0b582db42509824c47b82a
KONSTANTIN <kuznetsovkm@yandex.ru>
2026-02-04 06:58:13 +0300

    ДПБ исправил ущерб

--- calculations/app/_base_cost_for_damage.py
+++ calculations/app/_base_cost_for_damage.py
@@ -1,13 +1,16 @@
-def cost_large_tank(V):
-    return 85_000 * (V ** 0.62)
 
-def cost_small_tank(V):
-    return 22_000 * (V ** 0.78) + 45_000
 
 def approx_equipment_cost(V: float) -> float:
     if V <= 0:
         return 0.0
-    if V <= 100:
-        return cost_small_tank(V)
-    else:
-        return cost_large_tank(V)/1000
+
+    V0, y0 = 0.1, 100.0
+    Vb = 1000.0
+    m1 = 1.0
+
+    if V <= Vb:
+        return y0 + m1 * (V - V0)
+
+    yb = y0 + m1 * (Vb - V0)
+    m2 = (25000.0 - yb) / (10000.0 - Vb)
+    return yb + m2 * (V - Vb)
